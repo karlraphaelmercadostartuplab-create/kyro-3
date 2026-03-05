177c3198f3633024d1803874c15d2b145f6bff6f
Karl Raphael Mercado <karlraphael.mercado.startuplab@gmail.com>
2026-03-05 10:00:45 +0800

    search bar fix

--- resources/js/components/ui/search-input.tsx
+++ resources/js/components/ui/search-input.tsx
@@ -42,7 +42,7 @@ export function SearchInput({
             size="sm"
             onClick={() => {
               onChange('');
-              onSearch(true);
+              setTimeout(() => onSearch(true), 0);
             }}
             className="absolute right-1 top-1/2 transform -translate-y-1/2 h-6 w-6 p-0 text-muted-foreground hover:text-foreground"
           >
