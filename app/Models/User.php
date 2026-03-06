27e1a149c79c240b954df83814df024d60df49d0
KristianMDansangani <dansangani29@gmail.com>
2026-03-06 11:55:15 +0800

    Improve validation handling
    Implemented these 3
    -block reusing current password on change/reset,
    
    -add password history (last N passwords),
    
    -add true account lock after X failed logins with unlock time.

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\Permission\Models\Permission as ModelsPermission;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Str;
@@ -40,6 +41,8 @@ class User extends Authenticatable implements MustVerifyEmail
             'storage_limit',
             'is_disable',
             'is_enable_login',
+            'failed_login_attempts',
+            'locked_until',
             'creator_id',
             'created_by',
             'active_status',
@@ -67,6 +70,7 @@ class User extends Authenticatable implements MustVerifyEmail
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'locked_until' => 'datetime',
         ];
     }
 
@@ -120,6 +124,11 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->belongsTo(User::class, 'created_by');
     }
+    public function passwordHistories(): HasMany
+    {
+        return $this->hasMany(PasswordHistory::class);
+    }
+
 
     public static function CompanySetting($user_id)
     {
@@ -299,3 +308,6 @@ class User extends Authenticatable implements MustVerifyEmail
         }
     }
 }
+
+
+
