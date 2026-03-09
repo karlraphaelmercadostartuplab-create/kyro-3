fe8e17f4ee59f0a57210dc6a75c9ede8b64f518b
Karl Raphael Mercado <karlraphael.mercado.startuplab@gmail.com>
2026-03-09 17:44:01 +0800

    New Password must be new not old
    Ha

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -19,7 +19,16 @@ class PasswordController extends Controller
         if(Auth::user()->can('change-password-profile')){
             $validated = $request->validate([
                 'current_password' => ['required', 'current_password'],
-                'password' => ['required', Password::defaults(), 'confirmed'],
+                'password' => [
+                    'required',
+                    Password::defaults(),
+                    'confirmed',
+                    function (string $attribute, mixed $value, \Closure $fail) use ($request) {
+                        if (Hash::check($value, $request->user()->password)) {
+                            $fail("Password can't be the same as the current password.");
+                        }
+                    },
+                ],
             ]);
 
             $request->user()->update([
